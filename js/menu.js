c6bde480e726d77690824dadec4e466186b0c46d
Ivan Aleksanov <zloi.gremlin@gmail.com>
2012-11-21 20:36:08 +0400

    progress course

--- js/menu.js
+++ js/menu.js
@@ -6,20 +6,35 @@
  * To change this template use File | Settings | File Templates.
  */
 
-$(function(){
+$(function () {
+
+    elementsScroll = true;
+
+    $('#course-view .elemets_course').hover(function () {
+        var pop = $(this);
+        pop.find('.elements').fadeIn(0);
+        pop.find('.scroll').jScrollPane();
+        v = pop.find('.scroll .jspContainer');
+        pop.find('.scroll .jspContainer').css('height', pop.find('ol').height());
+    }, function () {
+        $(this).find('.elements').fadeOut(0);
+    });
+
+    $('#course-view .elemets_course .close').click(function(){
+        $(this).parent().fadeOut(0);
+
+    });
 
 
-
-
-    $('#course-view .navigate .expand>a').click(function(){
-        if (!$(this).is('.active')){
+    $('#course-view .navigate .expand>a').click(function () {
+        if (!$(this).is('.active')) {
             $(this).addClass('active');
-            $(this).parent().children('ul').slideDown(300, function(){
+            $(this).parent().children('ul').slideDown(300, function () {
                 $('#course-view .navigate .scroll').jScrollPane();
             });
         }
         else {
-            $(this).parent().find('ul').slideUp(350, function(){
+            $(this).parent().find('ul').slideUp(350, function () {
                 $('#course-view .navigate .scroll').jScrollPane();
             });
             $(this).parent().find('.expand>a').removeClass('active');
@@ -30,15 +45,15 @@ $(function(){
         }
         return false;
     });
-    $('#course-view .navigate .file').click(function(){
-        if (!$(this).is('.active')){
+    $('#course-view .navigate .file').click(function () {
+        if (!$(this).is('.active')) {
             $(this).addClass('active');
-            $(this).parent().children('.files').slideDown(300, function(){
+            $(this).parent().children('.files').slideDown(300, function () {
                 $('#course-view .navigate .scroll').jScrollPane();
             });
         }
         else {
-            $(this).parent().children('.files').slideUp(300, function(){
+            $(this).parent().children('.files').slideUp(300, function () {
                 $('#course-view .navigate .scroll').jScrollPane();
             });
             $(this).removeClass('active');
@@ -53,25 +68,25 @@ $(function(){
 
     $('#audience .scroll').jScrollPane();
 
-    $('#audience .user').live('click',function(){
+    $('#audience .user').live('click', function () {
         $(this).toggleClass('active');
-        if ($('#audience .user.active').size()){
+        if ($('#audience .user.active').size()) {
             $('#audience .head .del').fadeIn(150);
         }
         else {
             $('#audience .head .del').fadeOut(150);
         }
 
-  });
+    });
 
-    $('.messages #audience .user').live('click',function(){
-       $(this).removeClass('active');
+    $('.messages #audience .user').live('click', function () {
+        $(this).removeClass('active');
 
-  });
+    });
 
-    $('#audience .head .all').live('click',function(){
+    $('#audience .head .all').live('click', function () {
 
-        if ($(this).is('.active')){
+        if ($(this).is('.active')) {
             $(this).removeClass('active');
             $('#audience .user').removeClass('active');
             $('#audience .head .del').fadeOut(150);
@@ -82,21 +97,21 @@ $(function(){
             $('#audience .head .del').fadeIn(150);
         }
 
-  });
+    });
 
 
-    $('#course-settings .option label').click(function(){
+    $('#course-settings .option label').click(function () {
 
-         if ($(this).find('input').attr('checked')=='checked'){
+        if ($(this).find('input').attr('checked') == 'checked') {
 
             $(this).addClass('active');
         }
-         else {
+        else {
             $(this).removeClass('active');
         }
     });
 
-    $('#deadline').change(function(){
+    $('#deadline').change(function () {
         if ($(this).attr('checked') == 'checked') {
             $('#date-deadline').fadeIn(300);
         }
@@ -106,19 +121,19 @@ $(function(){
     });
 
 
-     $('#course-settings .parameters .radio label').click(function(){
+    $('#course-settings .parameters .radio label').click(function () {
         if (!$(this).is('.active')) {
             $('#course-settings .parameters .radio label').removeClass('active');
             $(this).addClass('active');
         }
     });
 
-    $('.course-users .head .status .choice').click(function(){
+    $('.course-users .head .status .choice').click(function () {
         $(this).toggleClass('active');
         $(this).next().fadeToggle(0);
     });
 
-    $('.course-users .head .status .choices li').click(function(){
+    $('.course-users .head .status .choices li').click(function () {
         $('.course-users .head .status .choices li').removeClass('active');
         var st = $(this).attr('class');
         $(this).addClass('active');
@@ -137,12 +152,12 @@ $(function(){
         }
     });
 
-     $('#course-settings .parameters.post li li').click(function(){
+    $('#course-settings .parameters.post li li').click(function () {
         if (!$(this).is('.active')) {
             $('#course-settings .parameters.post li li').removeClass('active');
             $(this).addClass('active');
             var link = $(this);
-            $('#course-settings .parameters li .textletter').slideUp(500, function(){
+            $('#course-settings .parameters li .textletter').slideUp(500, function () {
 
             });
             $(link.attr('rel')).slideDown(500);
@@ -154,12 +169,12 @@ $(function(){
         return false;
     });
 
-    $('#course-settings .parameters input').change(function(){
+    $('#course-settings .parameters input').change(function () {
         $('#course-settings a.save').addClass('active');
     });
 
 
-     $('#audience .user.messages .ac_bg').click(function(){
+    $('#audience .user.messages .ac_bg').click(function () {
         if (!$(this).is('.active')) {
             $('#audience .user.messages .ac_bg').removeClass('active');
             $(this).addClass('active');
@@ -167,24 +182,24 @@ $(function(){
             $($(this).parent().attr('rel')).fadeIn(100);
             $('#audience .rightContainer.last .scroll').jScrollPane();
         }
-         else {
+        else {
             $(this).removeClass('active');
             $('#audience .rightContainer.last .user-group').fadeOut(0);
         }
-      return false;
-  });
+        return false;
+    });
 
 
-    $('#SearchMessage').keydown(function(event) {
+    $('#SearchMessage').keydown(function (event) {
         var text;
-        setTimeout(function() {
+        setTimeout(function () {
             text = $('#SearchMessage').val();
             console.log(text);
         }, 50);
-        setTimeout(function() {
+        setTimeout(function () {
             if (text.length) {
                 $('.user-group .user').hide(0);
-                $('.user-group .user').each(function() {
+                $('.user-group .user').each(function () {
                     if ($(this).find('.name').text().toLowerCase().indexOf(text.toLowerCase()) + 1 || $(this).find('.mes').text().toLowerCase().indexOf(text.toLowerCase()) + 1 || $(this).find('em').text().toLowerCase().indexOf(text.toLowerCase()) + 1) {
                         $(this).show(0);
                     }
@@ -199,12 +214,12 @@ $(function(){
         }, 150);
     });
 
-    $('.yet').click(function(){
+    $('.yet').click(function () {
         $(this).parent().find('.expand').fadeToggle(0);
         return false;
     });
 
-    $('.cover').live('click',function(){
+    $('.cover').live('click', function () {
         $(this).parent().find('.expand').fadeOut(0);
         $(this).parent().find('.pops').fadeOut(0);
         $('.cover').remove();
@@ -212,7 +227,7 @@ $(function(){
 
 //    $('.pops .scroll').jScrollPane();
 
-    $('#course_assign h2 span a').click(function() {
+    $('#course_assign h2 span a').click(function () {
         var pop = $(this).parent().find('.pops');
         $('.pops').fadeOut(0);
         if (pop.css('display') == 'none') {
@@ -222,7 +237,7 @@ $(function(){
             var max = pop.find('ul').outerWidth();
             pop.css('width', (max + 40) + 'px');
             if (pop.find('ul').height() < 210) {
-                 pop.find('.scroll').css('height', (pop.find('ul').height()) + 'px');
+                pop.find('.scroll').css('height', (pop.find('ul').height()) + 'px');
             }
             $('.pops .scroll').jScrollPane();
 
@@ -234,7 +249,6 @@ $(function(){
     });
 
 
-  
     $('.fancy').fancybox();
 });
 
