aa3350a4d8c8ce8ecfa2fd88d10175584d785903
zloi.gremlin@gmail.com <zloi.gremlin@gmail.com>
2013-03-13 13:58:02 +0400

    new messages

--- js/new.js
+++ js/new.js
@@ -46,12 +46,14 @@ $(function () {
         $('#course-view-new-user #course-results .users .scroll').height($(window).height()-400);
         $('#course-view-new-user #audience .rightContainer.resize-1 .scroll').height($(window).height()-300);
         $('#course-view-new-user #audience .rightContainer.resize-2 .scroll').height($(window).height()-350);
+        $('#course-view-new-user #audience .rightContainer.resize-3 .scroll').height($(window).height()-450);
     });
 
 
     $('#course-view-new-user #course-results .users .scroll').height($(window).height()-400);
     $('#course-view-new-user #audience .rightContainer.resize-1 .scroll').height($(window).height()-300);
     $('#course-view-new-user #audience .rightContainer.resize-2 .scroll').height($(window).height()-350);
+    $('#course-view-new-user #audience .rightContainer.resize-3 .scroll').height($(window).height()-450);
 
     $('#course-view-new-user #course-results .notify li a').click(function(){
         $('#course-view-new-user #course-results .notify li').removeClass('active');
